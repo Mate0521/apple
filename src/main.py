868cc489e5006e2c4f077e89f4f4c270c4ef3bdf
Juancho507 <judaorgor0417@gmail.com>
2026-03-16 21:31:43 -0500

    Se arregla puerto para docker

--- src/main.py
+++ src/main.py
@@ -163,6 +163,6 @@ def home():
 # EJECUTAR SERVIDOR
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(host="0.0.0.0", port=port)
